b5a7eb40de83926534b7403ee20a7d1ee67f846b
Christoph <christoph286@googlemail.com>
2020-08-21 23:30:23 +0200

    a
    a

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: { default: Home, test: About }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: {
+       default: About
+    }
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
